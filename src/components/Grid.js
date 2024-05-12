e3bf69249a1d5d00ad0d724cf462853890e1c550
tamomsi <122281591+tamomsi@users.noreply.github.com>
2024-05-12 16:39:24 +0200

    integrated twoColumnTextSection component in Grid.js

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,10 +1,17 @@
+import React from "react";
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
+import TwoColumnTextSection from "./TwoColumnTextSection";
 
 const Grid = ({ blok }) => {
   return (
     <div className="grid grid-cols-3" {...storyblokEditable(blok)}>
-      {blok.columns.map((nestedBlok) => (
-        <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+      {blok.columns.map((nestedBlok, index) => (
+        // Conditionally render TwoColumnTextSection component in the second column
+        index === 1 ? (
+          <TwoColumnTextSection key={nestedBlok._uid} />
+        ) : (
+          <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+        )
       ))}
     </div>
   );
--- src/components/Page.js
+++ src/components/Page.js
@@ -1,9 +1,15 @@
+// Page.js
+
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
+import TwoColumnTextSection from "./TwoColumnTextSection";
 
 const Page = ({ blok }) => (
   <main className="text-center mt-4" {...storyblokEditable(blok)}>
-    {blok.body.map((nestedBlok) => (
-      <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+    {blok.body.map((nestedBlok, index) => (
+      <React.Fragment key={index}>
+        {index === 1 ? <TwoColumnTextSection /> : null}
+        <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+      </React.Fragment>
     ))}
   </main>
 );
